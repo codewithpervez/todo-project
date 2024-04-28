998737499adc14c6cb61621b198bba1358d1f6ed
codewithpervez <codewithpervez@gmail.com>
2024-04-28 05:59:21 -0700

    28-4-2024

--- todo/admin.py
+++ todo/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from.models import Task
 
+# Override some admin settings
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_complete', 'updated_at')
+    search_fields = ('task',)
+
 # Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
