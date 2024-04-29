91b46323de1b86f331f1846b0ffa2b75b5916102
codewithpervez <codewithpervez@gmail.com>
2024-04-29 11:32:07 +0500

    Complete working app

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import addTask, mark_as_done
+from .views import addTask, mark_as_done, delete_task, edit_task
 
 urlpatterns = [
     path('addTask', addTask, name='add-task'),
     path('mark_as_done/<int:pk>/', mark_as_done, name='mark-as-done'),
+    path('delete_task/<int:pk>/', delete_task, name='delete-task'),
+    path('edit_task/<int:pk>/', edit_task, name='edit-task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -17,3 +17,21 @@ def mark_as_done(request, pk):
     task.is_complete = True
     task.save()
     return redirect('home')
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        update = request.POST['task']
+        task.task = update
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'task': task,
+        }
+    return render(request, 'edit_task.html', context)
